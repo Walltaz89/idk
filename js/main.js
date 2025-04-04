7f5c6c239f47c525a76fc3f1963457429c2227eb
Walltaz89 <194965379+Walltaz89@users.noreply.github.com>
2025-04-03 23:44:09 -0500

    even more stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-// JS scripts placed here
+
+const rightImage = document.getElementById('rightswipe');
+
+
+rightImage.addEventListener('click', () => {
+  rightImage.classList.add('clicked'); 
+  rightImage.style.pointerEvents = 'none'; 
+});
+
+
+const leftImage = document.getElementById('leftswipe');
+
+
+leftImage.addEventListener('click', () => {
+  leftImage.classList.add('clicked'); 
+  leftImage.style.pointerEvents = 'none'; 
+});
+
