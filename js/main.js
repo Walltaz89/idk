1b03901b3f6e844189e32747a0f7ea8fa79698d2
Walltaz89 <194965379+Walltaz89@users.noreply.github.com>
2025-04-04 03:37:38 -0500

    meh

--- js/main.js
+++ js/main.js
@@ -16,3 +16,18 @@ leftImage.addEventListener('click', () => {
   leftImage.style.pointerEvents = 'none'; 
 });
 
+const main = document.getElementById("main");
+
+main.addEventListener("mousemove", function(e) {
+  const centerX = window.innerWidth / 2;
+  const centerY = window.innerHeight / 2;
+
+  const offsetX = (centerX - e.pageX) / 40;
+  const offsetY = (centerY - e.pageY) / 40;
+
+  document.querySelector(".green1").style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  document.querySelector(".green2").style.transform = `translate(${offsetX / 2}px, ${offsetY / 2}px)`;
+  document.querySelector(".green3").style.transform = `translate(${offsetX}px, ${offsetY / 2}px)`;
+  document.querySelector(".green4").style.transform = `translate(${offsetX / 1.5}px, ${offsetY / 3}px)`;
+});
+
